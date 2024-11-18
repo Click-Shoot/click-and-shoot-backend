d8a6b639d16fefaefd91927aca3dff4ebbe02415
Jordan <olivier.jordan21@gmail.com>
2024-11-18 11:22:29 +0100

    add image

--- src/models/tagModel.ts
+++ src/models/tagModel.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from 'mongoose';
 // Interface TypeScript pour un tag
 export interface ITag extends Document {
   label: string; // Libellé du tag
+  image: string; // URL de l'image associée au tag
 }
 
 // Schéma Mongoose pour les tags
 const tagSchema: Schema = new Schema({
   label: { type: String, required: true, unique: true }, // Le libellé doit être unique
+  image: { type: String, required: true }
 });
 
 export const TagModel = mongoose.model<ITag>('Tag', tagSchema);
